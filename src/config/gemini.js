1a4850204f07e2761a34fb9fab44132392115a76
Anmol <137643644+Anmol5478@users.noreply.github.com>
2025-04-02 12:16:31 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -44,7 +44,7 @@ import {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-pro",
+    model: "gemini-1.5-flash",
   });
   
   const generationConfig = {
